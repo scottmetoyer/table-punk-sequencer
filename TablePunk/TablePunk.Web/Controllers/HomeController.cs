4ba97c920c8acc0b4c1a885a0f96d459389f75fb
Scott Metoyer <scott.metoyer@ucr.edu>
2017-04-25 15:48:17 -0700

    Added .NET Core file upload functionality

--- TablePunk/TablePunk.Web/Controllers/HomeController.cs
+++ TablePunk/TablePunk.Web/Controllers/HomeController.cs
@@ -3,32 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace TablePunk.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private IHostingEnvironment _environment;
+
+        public HomeController(IHostingEnvironment env)
+        {
+            this._environment = env;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult Upload()
+        public IActionResult UploadFiles(IList<IFormFile> files)
         {
-            /*
-            if (Request.Files.Count == 1)
+            long size = 0;
+            foreach (var file in files)
             {
-                if (Request.Files[0].ContentLength > 0)
-                {
-                    string pathToSave = Server.MapPath("~/Content/Files/");
-                    string filename = Path.GetFileName(Request.Files[0].FileName);
-                    Request.Files[0].SaveAs(Path.Combine(pathToSave, filename));
-                }
+                var filename = ContentDispositionHeaderValue
+                                .Parse(file.ContentDisposition)
+                                .FileName
+                                .Trim('"');
+                filename = _environment.WebRootPath + $@"\{filename}";
+                size += file.Length;
 
+                using (FileStream fs = System.IO.File.Create(filename))
+                {
+                    file.CopyTo(fs);
+                    fs.Flush();
+                }
             }
-            */
-            return RedirectToAction("Index");
+
+            return this.RedirectToAction("Index");
         }
 
         public IActionResult Error()
